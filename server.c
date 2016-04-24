04aa2889ddbce44e10af4f7fcbce6354a70437a2
David Manzanarez <david.manzanarez.v@gmail.com>
2016-04-24 12:31:48 -0500

    seleccion de extensiones casi completa

--- server.c
+++ server.c
@@ -103,10 +103,10 @@ int serve(int s) {
 
     if(strncmp(token_header,"/",strlen(token_header))==0){
         strncpy(nombre_archivo_uri, "index.html", 11);
-        nombre_archivo_uri[10]='\0';
+        // nombre_archivo_uri[10]='\0';
     }else{
         strncpy(nombre_archivo_uri, token_header, strlen(token_header)+1);
-        nombre_archivo_uri[strlen(token_header)]='\0';
+        // nombre_archivo_uri[strlen(token_header)]='\0';
     }
 
     char *token_extension;
@@ -121,17 +121,7 @@ int serve(int s) {
     printf("EXTENSION ARCHIVO: %s\n", token_extension);
     printf("TIPO MIME: %s\n", tipoMime);
 
-    sleep(1);
-
-    sprintf(command, "HTTP/1.0 200 OK\r\n");
-    writeLine(s, command, strlen(command));
-
-    sprintf(command, "Date: Fri, 31 Dec 1999 23:59:59 GMT\r\n");
-    writeLine(s, command, strlen(command));
-
-
-    sprintf(command, "Content-Type: %s\r\n",tipoMime);
-    writeLine(s, command, strlen(command));
+    
 
     //test recuperar y calcular tamano de archivo
     FILE *da;
@@ -140,7 +130,6 @@ int serve(int s) {
     char *url_archivo="/home/ec2-user/var/www/html";
 
     char buff_url_completo[strlen(url_archivo)+strlen(nombre_archivo_uri)+strlen(token_extension)+2];
-
     strcat(buff_url_completo,url_archivo);
     strcat(buff_url_completo,"/");
     strcat(buff_url_completo,nombre_archivo_uri);
@@ -158,6 +147,20 @@ int serve(int s) {
     char *archivo = malloc(tamano+1);
     fread(archivo, tamano, 1, da);
     fclose(da);
+
+
+//LO QUE REGRESA DE FORMA EXPLICITA EL SERVIDOR
+    sleep(1);
+
+    sprintf(command, "HTTP/1.0 200 OK\r\n");
+    writeLine(s, command, strlen(command));
+
+    sprintf(command, "Date: Fri, 31 Dec 1999 23:59:59 GMT\r\n");
+    writeLine(s, command, strlen(command));
+
+
+    sprintf(command, "Content-Type: %s\r\n",tipoMime);
+    writeLine(s, command, strlen(command));
 
     printf("%s\n", archivo);
     printf("Tam archivo: %d\n", tamano);
@@ -215,128 +218,3 @@ int main() {
     sleep(1);
 
 }
-
-// #include <stdio.h>
-// #include <sys/types.h>
-// #include <sys/socket.h>
-// #include <netinet/in.h>
-// #include <netdb.h>
-// #include <string.h>
-// #include <arpa/inet.h>
-// #include <stdlib.h>
-
-
-// #define PORT 80
-// #define SIZE 8
-// #define MSGSIZE 1024
-
-// int readLine(int s, char *line, int *result_size) {
-//     int acum=0, size;
-//     char buffer[SIZE];
-
-//     while( (size=read(s, buffer, SIZE)) > 0) {
-//         if (size < 0) return -1;
-//         strncpy(line+acum, buffer, size);
-//         acum += size;
-//         if(line[acum-1] == '\n' && line[acum-2] == '\r') {
-//             break;
-//         } 
-//     }
-
-//     *result_size = acum;
-
-//     return 0;
-// }
-
-// int writeLine(int s, char *line, int total_size) {
-//     int acum = 0, size;
-//     char buffer[SIZE];
-
-//     if(total_size > SIZE) {
-//         strncpy(buffer, line, SIZE);
-//         size = SIZE;
-//     } else  {
-//         strncpy(buffer, line, total_size);
-//         size = total_size;
-//     }
-
-//     while( (size=write(s, buffer, size)) > 0) {
-//         if(size<0) return size;
-//         acum += size;
-//         if (acum >= total_size) break;
-
-//         size = ((total_size-acum)>=SIZE)?SIZE:(total_size-acum)%SIZE;
-//         strncpy(buffer, line+acum, size);
-//     }
-
-//     return 0;
-// }
-
-// int serve(int s) {
-//     char command[MSGSIZE];
-//     int size, r, nlc = 0;
-
-    // while(1) {
-    //     r = readLine(s, command, &size);
-    //     command[size-2] = 0;
-    //     size-=2;
-    //     printf("[%s]\n", command);
-    //     if(command[size-1] == '\n' && command[size-2] == '\r') {
-    //         break;
-    //     }
-    // }
-//     sleep(1);
-
-//     sprintf(command, "HTTP/1.0 200 OK\r\n");
-//     writeLine(s, command, strlen(command));
-
-//     sprintf(command, "Date: Fri, 31 Dec 1999 23:59:59 GMT\r\n");
-//     writeLine(s, command, strlen(command));
-
-//     sprintf(command, "Content-Type: text/plain\r\n");
-//     writeLine(s, command, strlen(command));
-
-//     sprintf(command, "Content-Length: 11\r\n");
-//     writeLine(s, command, strlen(command));
-
-//     sprintf(command, "\r\nHola mundo!");
-//     writeLine(s, command, strlen(command));
-// }
-
-// int main() {
-//     int sd, sdo, addrlen, size;
-//     struct sockaddr_in sin, pin;
-
-//     // 1. Crear el socket
-//     sd = socket(AF_INET, SOCK_STREAM, 0);
-
-//     memset(&sin, 0, sizeof(sin));
-//     sin.sin_family = AF_INET;
-//     sin.sin_addr.s_addr = INADDR_ANY;
-//     sin.sin_port = htons(PORT);
-
-//     // 2. Asociar el socket a un IP/puerto
-//     bind(sd, (struct sockaddr *) &sin, sizeof(sin));
-//     // 3. Configurar el backlog
-//     listen(sd, 5);
-
-//     addrlen = sizeof(pin);
-//     // 4. aceptar conexión
-//     while( (sdo = accept(sd, (struct sockaddr *)  &pin, &addrlen)) > 0) {
-//         if(!fork()) {
-//             printf("Conectado desde %s\n", inet_ntoa(pin.sin_addr));
-//             printf("Puerto %d\n", ntohs(pin.sin_port));
-
-//             serve(sdo);
-
-//             close(sdo);
-//             exit(0);
-//         }
-//     }
-//     close(sd);
-
-//     sleep(1);
-
-// }
-
-
