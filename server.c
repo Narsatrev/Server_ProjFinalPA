b727f09322bf275fb564ec08f35b34a72f5e39a2
David Manzanarez <david.manzanarez.v@gmail.com>
2016-04-26 11:15:14 -0500

    ahora no bajan multiples arhcivos de una misma pag...

--- server.c
+++ server.c
@@ -280,7 +280,7 @@ int main() {
 
         //Multithread
         pthread_t hiloCliente;
-        
+
         if (pthread_create(&hiloCliente , NULL, serve, sdo) != 0){
             openlog("ErrorCreacionNuevoThreadClinete", LOG_PID | LOG_CONS, LOG_USER);
             syslog(LOG_INFO, "Error: %s\n", strerror(errno));
@@ -289,6 +289,8 @@ int main() {
         }
 
         atexit(servidorCayo);
+
+         pthread_join(hiloCliente, NULL);
 
     }
     close(sd);
