5466724da12d06016ebea7d1c648c2a0ce9475ff
David Manzanarez <david.manzanarez.v@gmail.com>
2016-05-05 16:42:21 -0500

    grandes avances cgi

--- server.c
+++ server.c
@@ -363,7 +363,7 @@ int serve(int s) {
 
                     putenv("REQUEST_METHOD=GET");
                     putenv("REDIRECT_STATUS=True");
-                    putenv("QUERY_STRING=hola=a&mundo=b");
+                    putenv(token_b);
                     putenv("SCRIPT_FILENAME=test.php");
 
                     if(execlp("php-cgi", "php-cgi",url_completo, 0)<0){
