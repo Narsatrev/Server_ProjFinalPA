ff1d1edb2e9e1d5627d513e09635664666dff533
David Manzanarez <david.manzanarez.v@gmail.com>
2016-05-05 08:59:39 -0500

    grandes avances cgi

--- server.c
+++ server.c
@@ -268,7 +268,8 @@ int serve(int s) {
             free(archivo);
 
         }else{
-
+            printf("METODO: %d\n",mtodo);
+            if(metodo==0){
                 //Si no hay datos que requieran procesamiento, solo regresa un archivo estatico
                printf("SI EXISTE EL ARCHIVO YAY!!!\n");
 
@@ -302,7 +303,41 @@ int serve(int s) {
                         break;
                     }
                 }
+            }else{
+            int message_fd[2][2];
+                int i;
 
+                pipe(message_fd[READ]);
+                pipe(message_fd[WRITE]);
+
+                if(!fork()) {
+                    close(message_fd[READ][READ]);
+                    close(message_fd[WRITE][WRITE]);
+
+                    dup2(message_fd[READ][WRITE], 1);
+                    dup2(message_fd[WRITE][READ], 0);
+
+                    putenv("REQUEST_METHOD=GET");
+                    putenv("REDIRECT_STATUS=True");
+                    putenv("QUERY_STRING=hola=a&mundo=b");
+                    putenv("SCRIPT_FILENAME=test.php");
+
+                    execlp("php-cgi", "php-cgi", "test.php", 0);
+                }
+                close(message_fd[READ][WRITE]);
+                close(message_fd[WRITE][READ]);
+
+                FILE *fin = fdopen(message_fd[READ][READ], "r");
+                FILE *fout = fdopen(message_fd[WRITE][WRITE], "w");
+
+                char buffer[32];
+                int size = 0;
+                while(1) {
+                    if(feof(fin)) break;
+                    size = fread(buffer, 32, 1, fin);
+                    fwrite(buffer, 32, 1, stdout);
+                }            
+            }
     }    
     fclose(da);
 }
