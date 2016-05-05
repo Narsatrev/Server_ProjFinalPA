08e203946ed79b733ec55d415f0fd32a0729d2c4
David Manzanarez <david.manzanarez.v@gmail.com>
2016-05-05 12:46:26 -0500

    grandes avances cgi

--- server.c
+++ server.c
@@ -374,7 +374,7 @@ int serve(int s) {
                 sprintf(command, "\r\n");
                 writeLine(s, command, strlen(command));
 
-                while (read(temp,1,&c) > 0){
+                while (read(temp,&c,1) > 0){
                     write(s,&c,1);
                 }
             }
