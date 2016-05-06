a4add2b3ada098a4ec8ae0f31ca3c91396fc64ab
David Manzanarez <david.manzanarez.v@gmail.com>
2016-05-05 21:01:28 -0500

    peleando con POST...

--- server.c
+++ server.c
@@ -59,6 +59,7 @@ int readLine(int s, char *line, int *result_size) {
     while( (size=read(s, buffer, SIZE)) > 0) {
         if (size < 0) return -1;
         strncpy(line+acum, buffer, size);
+        printf("BUFFER: %s\n",buffer);
         acum += size;
         if(line[acum-1] == '\n' && line[acum-2] == '\r') {
             break;
