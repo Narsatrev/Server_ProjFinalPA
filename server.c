d705c7542f911255f9cf0c062d5da71611de38fb
David Manzanarez <david.manzanarez.v@gmail.com>
2016-05-06 12:16:55 -0500

    peleando con POST...

--- server.c
+++ server.c
@@ -100,7 +100,7 @@ int readLine(int s, char *line, int *result_size) {
                     token_pos2=strtok(token_pos1,"C");
                     char buff[50];
                     strncpy(buff,token_pos2,50);
-                    printf("TOKEN CHAK: %c",buff[0]);                
+                    printf("TOKEN CHAK: %s",buff);                
                 }   
                 banderaUbicacionContentLength=1; 
             }
