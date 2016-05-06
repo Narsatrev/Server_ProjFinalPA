ba607b046ce89a619d243a443af9eaa9be9a11da
David Manzanarez <david.manzanarez.v@gmail.com>
2016-05-06 13:23:33 -0500

    peleando con POST...

--- server.c
+++ server.c
@@ -115,7 +115,7 @@ int readLine(int s, char *line, int *result_size) {
             }else{
                 int t=0;
                 while(t<longitudPost){
-                    puts("%c",line[t]);
+                    puts(line[t]);
                     t++;
                 }                
             } 
