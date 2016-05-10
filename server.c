18b1c492807968708a35bfbea6ad1122c7177124
David Manzanarez <david.manzanarez.v@gmail.com>
2016-05-10 18:38:46 -0500

    opcion para demonizar

--- server.c
+++ server.c
@@ -25,44 +25,33 @@ int sdo;
 
 char buffFecha[1000];
 
-void crearDemonio(){
+static void crearDemonio(){
     pid_t pid;
 
-    /* Fork off the parent process */
     pid = fork();
 
-    /* An error occurred */
     if (pid < 0)
         exit(EXIT_FAILURE);
 
-    /* Success: Let the parent terminate */
     if (pid > 0)
         exit(EXIT_SUCCESS);
 
-    /* On success: The child process becomes session leader */
     if (setsid() < 0)
         exit(EXIT_FAILURE);
 
-    /* Catch, ignore and handle signals */
-    //TODO: Implement a working signal handler */
     signal(SIGCHLD, SIG_IGN);
     signal(SIGHUP, SIG_IGN);
 
-    /* Fork off for the second time*/
     pid = fork();
 
-    /* An error occurred */
     if (pid < 0)
         exit(EXIT_FAILURE);
 
-    /* Success: Let the parent terminate */
     if (pid > 0)
         exit(EXIT_SUCCESS);
 
-    /* Set new file permissions */
     umask(0);
 
-    /* Close all open file descriptors */
     int x;
     for (x = sysconf(_SC_OPEN_MAX); x>0; x--)
     {
@@ -637,7 +626,7 @@ int main(int argc, char **argv) {
     }
 
     //si la persona indica que quiere demonziar el proceso
-    if(demonizar){
+    if(demonizar == 1){
         crearDemonio();
     }
 
